5043286b9a88d8b9317f9c3e4ac63c60c5e51151
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-07-26 22:23:19 +0700

    Creating folder

--- src/test2.cpp
+++ src/test2.cpp
@@ -17,17 +17,22 @@ int main()
 
 
     struct passwd *pw = getpwuid(getuid());
-    const char *homedir = pw->pw_dir;
-    std::string str = homedir;
-    str += "/Flight Log";
-    std::cout << str << std::endl;
+    const char *home_char = pw->pw_dir;
+    std::string home_dir = home_char;
+    std::string flight_dir = "Flight Log";
+    std::string temp_1 = home_dir + "/" + flight_dir;
+    std::cout << temp_1 << std::endl;
 
-    bool is_exist = boost::filesystem::exists(str);
+    bool is_exist = boost::filesystem::exists(temp_1);
 
     if(is_exist)
         std::cout << "Exist" << std::endl;
     else
-        std::cout << "Exist" << std::endl;
+    {
+        std::cout << "Not Exist" << std::endl;
+        temp_1 = "mkdir " + home_dir + "/'" + flight_dir +"'";
+        system(temp_1.c_str());
+    }
 
     return 0;
 }
