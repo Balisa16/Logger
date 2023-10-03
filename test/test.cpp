6d108caf76c275b36ad008658358201010b1c44c
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-10-03 15:53:21 +0700

    Stuck using mutithread

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #include <Logger.hpp>
 #include <thread>
-
+#include <vector>
+#include <utility>
 using namespace EMIRO;
 
 int main()
@@ -8,14 +9,18 @@ int main()
     Logger lg;
     lg.init("test", FileType::CSV);
     lg.start(true);
-    for (size_t i = 0; i < 10; i++)
+    std::vector<std::thread> th_vect = std::vector<std::thread>();
+    for (int i = 0; i < 10; i++)
     {
-        lg.write_show(LogLevel::INFO, "Message %d", i);
-        lg.write_show(LogLevel::WARNING, "Message %d", i);
-        lg.write_show(LogLevel::ERROR, "Message %d", i);
-        std::chrono::milliseconds sleepDuration(100);
-        std::this_thread::sleep_for(sleepDuration);
+        LogLevel ll = LogLevel::INFO;
+        const char *word = "Message %d";
+        int a = 1;
+
+        std::thread th(lg.write_show, ll, word, a);
+        th_vect.push_back(th);
     }
+    for (int i = 0; i < th_vect.size(); ++i)
+        th_vect[i].join();
     
     lg.finish();
     return 0;
